074704c65fdb8c2160641555c729b7ee44749d5b
MalyanovAlexander <malyanov_91@mail.ru>
2021-11-26 16:43:57 +0300

    Занятие 4: ViewComponent для брендов + рефакторинг

---
+++ WebStore/Models/BrandViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Models
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Order { get; set; }
+
+        /// <summary>
+        ///Количество товаров бренда 
+        /// </summary>
+        public int ProductsCount { get; set; }
+    }
+}
