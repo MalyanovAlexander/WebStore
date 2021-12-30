724ec9a7dbd620d938dfebaf851058da106d0be1
MalyanovAlexander <malyanov_91@mail.ru>
2021-12-30 19:16:32 +0300

    bugfix: admin area only for admin

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ using WebStore.Infrastructure.Interfaces;
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles="Admin")]
     public class HomeController : Controller
     {
         private readonly IProductService _productdata;
