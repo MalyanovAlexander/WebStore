a2507952a72535ff9d5b63819419af2ad99fb033
MalyanovAlexander <malyanov_91@mail.ru>
2021-12-30 16:00:48 +0300

    Занятие 8: область админки (admin area)

---
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DomainNew.Filters;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productdata;
+
+        public HomeController(IProductService productData)
+        {
+            _productdata = productData;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            return View(_productdata.GetProducts(new ProductFilter()));
+        }
+    }
+}
